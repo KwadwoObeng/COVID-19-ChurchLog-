fab80f5916e79e8372478b5b207fb826331c4c4e
Kwadwo-Obeng <obengafrane.oa@gmail.com>
2020-07-04 22:47:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 # static folder will contain the css and images and other decorative elements
 # templates folder will contain the html codes
+# / -home page, /userform -user books service here
+# /admin -admin can view service list here
+
+
 from churchRegistration import app
-from flask import Flask
+from flask import Flask, render_template
 
 @app.route('/')
 def index():
     return "Hello World"
+
+
+@app.route('/userform')
+def userform():
+    return
+
+
+@app.route('/admin')
+def admin():
+    return
+
+
+@app.route('/admin/AdminAddService')
+def add_service():
+    return
+
 
 if __name__ == '__main__':
     app.run(debug = True)
