205948f21fc09b1eef6463c4e81c5aa74a38848d
akackon <adrianackon@rocketmail.com>
2020-07-16 04:38:27 +0000

    created user model

--- churchRegistration/models.py
+++ churchRegistration/models.py
@@ -12,3 +12,12 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(), unique = True)
     email = db.Column(db.String(64), unique = True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
