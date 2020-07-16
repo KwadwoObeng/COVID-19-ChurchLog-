3960e296fe530b6f98dd7d55965cd4a8e339d434
akackon <adrianackon@rocketmail.com>
2020-07-16 04:51:31 +0000

    removed integerRangeField from forms

--- churchRegistration/forms.py
+++ churchRegistration/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerRangeField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.fields.html5 import DateField, TimeField
 from wtforms.validators import DataRequired, Email, EqualTo
 from wtforms import ValidationError
