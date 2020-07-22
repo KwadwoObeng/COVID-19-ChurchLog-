f5fa7e810986eafef526ab62b266f7d82978b181
Kwadwo-Obeng <obengafrane.oa@gmail.com>
2020-07-22 18:41:35 +0000

    Update models.py

--- churchRegistration/models.py
+++ churchRegistration/models.py
@@ -1,6 +1,7 @@
-from churchRegistration import db, login_manager
+from churchRegistration import db, login_manager,app
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -23,6 +24,19 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id':self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
 
 class ChurchService(db.Model):
